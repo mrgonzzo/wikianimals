3f0cbfe13892e3f89e7f3a8ee03a1401cf3c7f03
mrgonzzo <mr.gonzzo@gmail.com>
2017-04-23 19:39:06 +0200

    v3 more animals

--- app/services/animal-factory.js
+++ app/services/animal-factory.js
@@ -4,9 +4,13 @@
         var module = {};
         var self = module;
         var animalsarray = [
-            { id: 1, name: "ornitorrinco", ciname: "Ornithorhynchus anatinus", picture: "app/resources/Ornitorrinco.jpg" },
-            { id: 2, name: "pez rosado con manos", ciname: "Brachiopsilus dianthus", picture: "app/resources/pez-manos-rosa.jpg" },
-            { id: 3, name: "topo nariz de estrella", ciname: "Condylura cristata", picture: "app/resources/topo-nariz-estrella.jpg" },
+            { id: 1, name: "ornitorrinco", ciname: "Ornithorhynchus anatinus", picture: "Ornitorrinco.jpg" },
+            { id: 2, name: "pez rosado con manos", ciname: "Brachiopsilus dianthus", picture: "pez-manos-rosa.jpg" },
+            { id: 3, name: "topo nariz de estrella", ciname: "Condylura cristata", picture: "topo-nariz-estrella.jpg" },
+            { id: 4, name: "Cangrejo yeti", ciname: "Kiwa hirsuta", picture: "cangrejo.jpg" },
+            { id: 5, name: "oso de agua", ciname: "Tardigrada", picture: "tardigrada.gif" },
+            { id: 6, name: "Lagarto cornudo", ciname: "Phrynosoma cornutum", picture: "lagarto.gif" },
+            { id: 7, name: "Gato esfinge", ciname: "Sphynx", picture: "Sphynx.gif" },
         ];
 
         module.getAllAnimals = function () {
@@ -14,7 +18,7 @@
         };
 
         module.getAnimalById = function (idanimal) {
-            console.log("idanimal: "+idanimal);
+            console.log("idanimal: " + idanimal);
             for (var i = 0; i < animalsarray.length; i++) {
                 if (idanimal == animalsarray[i].id) {
                     animal = animalsarray[i];
